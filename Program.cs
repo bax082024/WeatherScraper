d6f046a0d1b9a31feb89f3ea35a176bf63ee33f1
bax082024 <bax082024@gmail.com>
2024-10-27 22:46:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    String url = ""
+    var httpClient = new HttpClient();
+    var html = httpClient.GetStringAsync(url).Result;
+
+  }
+
+}

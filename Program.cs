a0800ff935ef0bb1dca39487dc36e0680d5a11d4
bax082024 <bax082024@gmail.com>
2024-10-27 22:48:52 +0100

    added url

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
 {
   static void Main(string[] args)
   {
-    String url = ""
+    String url = "https://www.yr.no/nb/v%C3%A6rvarsel/daglig-tabell/1-92416/Norge/Vestland/Bergen/Bergen";
     var httpClient = new HttpClient();
     var html = httpClient.GetStringAsync(url).Result;
     var htmlDocument = new HtmlDocument();

74c7584da6cd804167ac9050f0f055c8155c16da
bax082024 <bax082024@gmail.com>
2024-10-27 22:55:48 +0100

    added temperature class

--- Program.cs
+++ Program.cs
@@ -6,11 +6,22 @@ class Program
 {
   static void Main(string[] args)
   {
+    // send request to yr
     String url = "https://www.yr.no/nb/v%C3%A6rvarsel/daglig-tabell/1-92416/Norge/Vestland/Bergen/Bergen";
     var httpClient = new HttpClient();
     var html = httpClient.GetStringAsync(url).Result;
     var htmlDocument = new HtmlDocument();
     htmlDocument.LoadHtml(html);
+
+
+    // Get temperature
+    var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='temperature temperature--warm-primary']");
+    var temperature = temperatureElement.InnerText.Trim();
+
+    // Get the Conditions
+
+
+    // Location
 
   }
 
